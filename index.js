b26adf29170475e8fa0778607dcad3e5f19b28fb
Aditya Koshta <adityakoshta03@gmail.com>
2024-10-06 05:36:08 -0400

    Added files

---
+++ index.js
@@ -0,0 +1,11 @@
+let playBtn = document.getElementById('play-pause-btn');
+
+playBtn.addEventListener('click', function() {
+    alert('Play Button Clicked!');
+})
+
+let body = document.querySelector('body');
+
+body.addEventListener('click', function() {
+    alert('Sorry Nothings is Working!');
+})
